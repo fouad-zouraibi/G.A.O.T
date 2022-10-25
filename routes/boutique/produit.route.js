20c0149490d0cdd37535164fa29ad1de76714922
haider jouini <haider.j@xelero.io>
2022-10-25 22:11:33 +0200

    fix get by id product

--- routes/boutique/produit.route.js
+++ routes/boutique/produit.route.js
@@ -1,88 +1,74 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 // Middleware
-const authMiddle = require('../../middleware/auth.middleware')
-const rolesMiddle = require('../../middleware/roles.middleware')
-const { uploadProduit } = require('../../middleware/upload.middleware')
+const authMiddle = require('../../middleware/auth.middleware');
+const rolesMiddle = require('../../middleware/roles.middleware');
+const {uploadProduit} = require('../../middleware/upload.middleware');
 
-const produitController = require('../../controllers/boutique/produit.controller')
+const produitController = require('../../controllers/boutique/produit.controller');
 
-router.use(express.json())
-router.use(express.urlencoded({ extended: false }))
+router.use(express.json());
+router.use(express.urlencoded({extended: false}));
+
+
+
 
 // ***************** Api Register for produit *****************
-router.post('/add', uploadProduit.single('image'), (req, res) => {
-    produitController
-        .add(req)
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
+router.post('/add',uploadProduit.single('image'),(req, res) => {
+
+  produitController.add(req).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
 
 // ***************** Api update for user *****************
 router.put('/update/:id', (req, res) => {
-    produitController
-        .update(req)
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
+  produitController.update(req).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
 
 // ***************** Api delete for user *****************
 router.delete('/delete/:id', (req, res) => {
-    produitController
-        .delete(req)
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
+  produitController.delete(req).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
+
 
 // ***************** Api get   by id  *****************
 router.get('/get/:id', (req, res) => {
-    produitController
-        .get(req.params.id)
-        .then((data) => {
-            res.json(data)
-            //console.log("ok");
-            //res.render('pages/homepage',{'data': data})
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
+  produitController.get(req.params.id).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
 
 // ***************** Api get   by title  *****************
 router.get('/getname/:titre', (req, res) => {
-    produitController
-        .get(req.params.titre)
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
+  produitController.getByName(req.params.titre).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
 
 // ***************** Api list users *****************
 router.get('/list/:page?/:limit?', (req, res) => {
-    produitController
-        .getList(req)
-        .then((data) => {
-            res.json(data)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
+  produitController.getList(req).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+});
 
-module.exports = router
+
+module.exports = router;
